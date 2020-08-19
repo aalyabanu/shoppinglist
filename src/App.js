f40dce711fdfc44acf618c0e99bf5fe35f4febd8
Aalya <aalya.banu@gmail.com>
2020-08-19 12:43:43 +0100

    map error

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Add from "./Add";
+import Links from "./Links";
+import VisibleItemList from "./VisibleItemList";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Navbar bg="info" variant="dark">
+          <Navbar.Brand>React Shopping List</Navbar.Brand>
+        </Navbar>
+        <Container>
+          <Add />
+          <Links />
+          <VisibleItemList />
+        </Container>
+      </>
+    );
+  }
+}
+
+export default App;
