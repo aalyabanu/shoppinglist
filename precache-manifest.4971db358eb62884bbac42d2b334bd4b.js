3a6e64eaa3040c5416ad27fe41cb0298fab43b2c
Aalya <aalya.banu@gmail.com>
2020-08-25 14:53:57 +0100

    Updates

---
+++ precache-manifest.4971db358eb62884bbac42d2b334bd4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b1a8e5495a11f2346bb77132ea2cfe",
+    "url": "/shoppinglist/index.html"
+  },
+  {
+    "revision": "91a5757fbfe34e2068d5",
+    "url": "/shoppinglist/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "81213ee265263b8e9516",
+    "url": "/shoppinglist/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "91a5757fbfe34e2068d5",
+    "url": "/shoppinglist/static/js/2.d3dee4f5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shoppinglist/static/js/2.d3dee4f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81213ee265263b8e9516",
+    "url": "/shoppinglist/static/js/main.41cb6dbb.chunk.js"
+  },
+  {
+    "revision": "4741dda35ccd31f5d401",
+    "url": "/shoppinglist/static/js/runtime-main.939ef0e2.js"
+  }
+]);
